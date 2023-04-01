a082da2c1632cbd40a8f9066561ca458ae9d5c8c
filipkustoch <filip.kustoch1@gmail.com>
2023-04-01 23:20:59 +0200

    Modified DB class

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -5,7 +5,11 @@ require_once("new_config.php");
 class Database
 {
 
-    private $connection;
+    public $connection;
+
+    function __construct(){
+        $this->open_db_connection();
+    }
     public function open_db_connection()
     {
         $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
