c47ea8ff0260435f4db7d7d27b3423c425816924
filipkustoch <filip.kustoch1@gmail.com>
2023-04-05 22:59:18 +0200

    Testing query

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -7,6 +7,22 @@
                 Blank Page
                 <small>Subheading</small>
             </h1>
+
+            <?php
+
+            $database = new Database();
+
+            $sql = "SELECT * FROM users";
+
+            $result = $database->query($sql);
+            $user_found = mysqli_fetch_array($result);
+
+            echo $user_found['username'];
+
+            ?>
+
+
+
             <ol class="breadcrumb">
                 <li>
                     <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
